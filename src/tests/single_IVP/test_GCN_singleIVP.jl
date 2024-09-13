3d4bdf40a4d1ce1403e32642869be9a3b70689d0
Davide Amato <d.amato@imperial.ac.uk>
2024-09-13 11:17:32 +0100

    Add constants, input, and skeleton of test script

---
+++ src/tests/single_IVP/test_GCN_singleIVP.jl
@@ -0,0 +1,21 @@
+#= Test script for an individual orbit propagation with the GCN method =#
+
+include("Inputs_IVP_test.jl")
+include("Constants_test.jl")
+
+include("../../SpectralBases.jl")
+include("../../SpectralSolver.jl")
+include("../../CowellPropagator.jl")
+include("../../IVP_Solvers.jl")
+
+include("../../Perturbations_Geopotential.jl")
+
+include("../../DataHandling.jl")
+
+
+# Set up ICs and timespan
+x0 = [a / DU, e, i * π / 180, RAAN * π / 180, omega * π / 180, theta * π / 180]
+t_span = time_span_IVP ./ TU
+sys_size = size(x0)[1]  # = N in the GCN paper. Size of the state.
+
+
